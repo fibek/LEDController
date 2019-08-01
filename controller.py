0f16251386e8d5a377a568c15183964fae292c3f
Igor <igorfib6@gmail.com>
2019-08-02 00:19:15 +0200

    Mode menu added (now working correctly)

--- controller.py
+++ controller.py
@@ -55,7 +55,47 @@ def rainbow(etime):
 def r_interrupt():
     print('Intererrupt')
     ser.write(b'0')
-    
+
+def menu(event):
+    if modes.get() == 'Static color':
+        try:
+            frame2.grid_forget()
+        except:
+            print('ni wim co to kurwa ten jebany frame2')
+        #frame1 = Frame(root)
+        print(modes.current(), modes.get())
+        Label(frame1, text='\nSet static color', font=('none', 14), bg="#181b28", fg="#d3dae3") .grid(row=10, column=1)
+        Label(frame1, text='R: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=11, column=1, sticky=S)
+        entryr = Entry(frame1, width=4, bg='white', fg='black', borderwidth=0) 
+        entryr.grid(row=11, column=1, sticky=E)
+        Label(frame1, text='G: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=12, column=1, sticky=S)
+        entryg = Entry(frame1, width=4, bg='white', fg='black', borderwidth=0) 
+        entryg.grid(row=12, column=1, sticky=E)
+        Label(frame1, text='B: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=13, column=1, sticky=S)
+        entryb = Entry(frame1, width=4, bg='white', fg='black', borderwidth=0) 
+        entryb.grid(row=13, column=1, sticky=E)
+        Label(frame1, text='\n', font=('none', 3), bg="#181b28"). grid(row=14)
+        Button(frame1, text='Set', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1, command=lambda: statclr(entryr.get(), entryg.get(), entryb.get())) .grid(row=15, column=1, sticky=S)
+        frame1.grid(row=5, column=0)
+        #frame2 = Frame(root)
+    elif modes.get() == 'Rainbow':
+        try:
+            frame1.grid_forget()
+        except:
+            print('ni wim co to kurwa ten jebany frame1')
+        #frame2 = Frame(root)
+        print(modes.current(), modes.get())
+        Label(frame2, text='\nRainbow', font=('none', 14), fg="#d3dae3", bg="#181b28") .grid(row=18, column=1, sticky=W)
+        Label(frame2, text='Unlimited: time = 0', font=('none', 10), bg="#181b28", fg="#d3dae3") .grid(row=19, column=1, sticky=W) 
+        Label(frame2, text='Time (s): ', font=('none', 12), fg="#d3dae3", bg="#181b28") .grid(row=20, column=1, sticky=S)
+        entrytime = Entry(frame2, width=4, bg='white', fg='black', borderwidth=0)
+        entrytime.grid(row=20, column=1, sticky=E)
+        Label(frame2, text='\n', font=('none', 3), bg="#181b28", fg="#d3dae3"). grid(row=21)
+        Button(frame2, text='Set', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1, command=lambda: rainbow(entrytime.get())) .grid(row=22, column=1, sticky=S)
+        Button(frame2, text='Stop', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1,  command=lambda: r_interrupt()) .grid(row=22, column=2, sticky=W)
+        frame2.grid(row=5, column=0)
+        #frame2 = Frame(root)
+
 root = Tk()
 root.title("Python LED Controller")
 root.geometry("633x487")
@@ -66,7 +106,7 @@ Label(root, text='\t \t \t', bg="#181b28", fg="#d3dae3") .grid(row=0, column=0)
 Label(root, text='Python LED Controller', font=('none', 22), bg="#181b28", fg="#d3dae3") .grid(row=2, column=2)
 Label(root, text='Available modes:', font=('none', 18), bg="#181b28", fg="#d3dae3") .grid(row=3, column=2)
 
-modes = ttk.Combobox(root, state='readonly', font=('none', 12),
+modes = ttk.Combobox(root, state='readonly', font=('none', 12), 
                      values=[
                              "Rainbow",
                              "Static color",
@@ -76,30 +116,8 @@ modes.current(2)
 
 print(modes.current(), modes.get())
 
-
-def menu(event):
-    if modes.get() == 'Static color':
-        Label(root, text='\nSet static color', font=('none', 14), bg="#181b28", fg="#d3dae3") .grid(row=10, column=1)
-        Label(root, text='R: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=11, column=1, sticky=S)
-        entryr = Entry(root, width=4, bg='white', fg='black', borderwidth=0) 
-        entryr.grid(row=11, column=1, sticky=E)
-        Label(root, text='G: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=12, column=1, sticky=S)
-        entryg = Entry(root, width=4, bg='white', fg='black', borderwidth=0) 
-        entryg.grid(row=12, column=1, sticky=E)
-        Label(root, text='B: ', font=('none', 12), bg="#181b28", fg="#d3dae3") .grid(row=13, column=1, sticky=S)
-        entryb = Entry(root, width=4, bg='white', fg='black', borderwidth=0) 
-        entryb.grid(row=13, column=1, sticky=E)
-        Label(root, text='\n', font=('none', 3), bg="#181b28"). grid(row=14)
-        Button(root, text='Set', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1, command=lambda: statclr(entryr.get(), entryg.get(), entryb.get())) .grid(row=15, column=1, sticky=S)
-    elif modes.get() == 'Rainbow':
-        Label(root, text='\nRainbow', font=('none', 14), fg="#d3dae3", bg="#181b28") .grid(row=18, column=1, sticky=W)
-        Label(root, text='Unlimited: time = 0', font=('none', 10), bg="#181b28", fg="#d3dae3") .grid(row=19, column=1, sticky=W) 
-        Label(root, text='Time(s): ', font=('none', 12), fg="#d3dae3", bg="#181b28") .grid(row=20, column=1, sticky=S)
-        entrytime = Entry(root, width=4, bg='white', fg='black', borderwidth=0)
-        entrytime.grid(row=20, column=1, sticky=E)
-        Label(root, text='\n', font=('none', 3), bg="#181b28", fg="#d3dae3"). grid(row=21)
-        Button(root, text='Set', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1, command=lambda: rainbow(entrytime.get())) .grid(row=22, column=1, sticky=S)
-        Button(root, text='Stop', fg='white', width=4, font=('none', 13), bg="#181b28", borderwidth=1,  command=lambda: r_interrupt()) .grid(row=22, column=2, sticky=W)
+frame1 = Frame(root)
+frame2 = Frame(root)
 
 modes.bind('<<ComboboxSelected>>', menu)
 
